7acd417863d66cef811404658a21bb773e9da88f
Laura Dietz <dietz@smart-cactus.org>
2026-03-08 15:55:49 -0400

    Fix it

--- flake.nix
+++ flake.nix
@@ -4,29 +4,37 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-gl-host.url = "github:numtide/nix-gl-host";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-gl-host }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate = pkg: true;
+            #pkgs.lib.strings.hasInfix "cuda" (pkgs.lib.getName pkg);
+        };
         python = pkgs.python311;
+        pythonEnv =
+          pkgs.python3.withPackages (ps: [
+          ]);
       in {
         devShells.default =
-          let base = pkgs.appimageTools.defaultFhsEnvArgs;
-          in (pkgs.buildFHSEnv (base // {
-            name = "FHS";
-            targetPkgs = pkgs: (with pkgs; [
-              gcc glibc zlib
-              python3
+          pkgs.mkShell {
+            name = "python-dev";
+            packages = with pkgs; [
+              pythonEnv
               python3Packages.uv
               git-lfs
               screen
               bashInteractive
               tmux
-            ]);
-            runScript = "bash $@";
-            extraOutputsToInstall = [ "dev" ];
-          })).env;
+
+              cudaPackages.cudatoolkit
+              cudaPackages.cudnn
+              nix-gl-host.defaultPackage.${system}
+            ];
+          };
       });
 }
