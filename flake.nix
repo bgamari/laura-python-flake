9081569a22fe25e0b1571128bd7e8726f80ce41d
Ben Gamari <ben@smart-cactus.org>
2026-03-09 12:42:26 -0400

    Add libstdc++

--- flake.nix
+++ flake.nix
@@ -34,7 +34,12 @@
               cudaPackages.cudatoolkit
               cudaPackages.cudnn
               nix-gl-host.defaultPackage.${system}
+
+              stdenv.cc.cc.lib
             ];
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+            '';
           };
       });
 }
